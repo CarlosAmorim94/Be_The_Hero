805fdab305c1c53bb0b0f69290936c0af2908afb
Carlos <carlos.av.amorim@gmail.com>
2022-03-05 17:56:00 -0300

    Add cors

--- BackEnd/src/index.js
+++ BackEnd/src/index.js
@@ -1,7 +1,16 @@
 const express = require('express');
+const cors = require('cors');
 const routes = require('./routes');
 
 const app = express();
+
+app.use(cors());
+
+/* Utilizando cors em produção:
+app.use(cors({
+  origin: 'http://LinkDoFrontEndLiberado.com.br'
+}));
+*/
 
 app.use(express.json());
 app.use(routes);
