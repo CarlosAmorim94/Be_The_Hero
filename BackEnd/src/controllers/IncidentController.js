03de26dba68ca55fd449f6401ef3bd61ce74eb5f
Carlos <carlos.av.amorim@gmail.com>
2022-03-04 21:52:44 -0300

    Created delete routes

--- BackEnd/src/controllers/IncidentController.js
+++ BackEnd/src/controllers/IncidentController.js
@@ -4,7 +4,6 @@ const connection = require('../database/connection');
 module.exports = {
 
   async index(request, response) {
-
     const incidents = await connection('incidents').select('*');
 
     response.json({ incidents });
@@ -25,7 +24,22 @@ module.exports = {
     return response.json({ id });
   },
 
+  async delete(request, response) {
+    const { id } = request.params;
+    const ong_id = request.headers.authorization;
 
+    const incident = await connection('incidents')
+      .where('id', id)
+      .select('ong_id')
+      .first();
 
+    if (incident.ong_id !== ong_id) {
+      return response.status(401).json({ error: 'Operation not permitted.' });
+    };
+
+    await connection('incidents').where('id', id).delete();
+
+    return response.status(204).send();
+  },
 
 };
