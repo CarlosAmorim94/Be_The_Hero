026adafcae0a053213a6bb5a953a99c6bee00c90
Carlos <carlos.av.amorim@gmail.com>
2022-03-09 21:18:59 -0300

    Add validations

--- BackEnd/src/index.js
+++ BackEnd/src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const routes = require('./routes');
+const { errors } = require('celebrate')
 
 const app = express();
 
@@ -14,5 +15,6 @@ app.use(cors({
 
 app.use(express.json());
 app.use(routes);
+app.use(errors());
 
 app.listen(3333);
